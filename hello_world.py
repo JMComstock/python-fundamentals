b344f48be4bc8ff3f15bff775a37e46ba9c4b66c
JMComstock <jasoncomstock11@gmail.com>
2022-05-27 12:15:10 -0500

    first commit

---
+++ hello_world.py
@@ -0,0 +1,38 @@
+print("Hello World")
+x = "Hello Python"
+print(x)
+y = 42
+print(y)
+first_name = "Jason"
+last_name = "Comstock"
+age = 40
+print(f"My name is {first_name} {last_name} and I am {age} years old.")
+print("My name is {} {} and I am {} years old.".format(first_name, 
+last_name, age))
+
+hw = "Hello %s" % "world"
+py = "I love Python %d" % 3
+print(hw)
+print(py)
+
+name = "Jason"
+age = 40
+print("My name is %s and I'm %d" % (name, age))
+
+x = "Hello World!"
+print(x.title())
+
+print("Hello World")
+name = "Noelle"
+print("Hello ", name)
+print("Hello " + name)
+name = "42"
+print("Hello ", name)
+print("Hello " + name)
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+
+print("I love to eat {} and {}.".format(fave_food1, fave_food2))
+print(f"I love to eat {fave_food1} and {fave_food2}.")
+
